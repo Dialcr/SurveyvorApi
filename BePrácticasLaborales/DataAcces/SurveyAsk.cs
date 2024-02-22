a6686a5c6f7209155a99252ee0a67882db23fa2e
Diego Alfonso Criado <diego.ac@factory.generalsoftwareinc.com>
2024-02-22 00:12:36 -0500

    save changes

---
+++ BePrácticasLaborales/DataAcces/SurveyAsk.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BePrácticasLaborales.DataAcces;
+
+public class SurveyAsk
+{
+    [Key]
+    public int Id { get; set; }
+    
+    [Key]
+    public int SurveyId { get; set; }
+    [ForeignKey(nameof(SurveyId))]
+    public Survey Survey { get; set; }
+    
+    // todo: posibles respuestas para las preguntas  
+}
---
+++ BePrácticasLaborales/DataAcces/SurveyResponse.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BePrácticasLaborales.DataAcces;
+
+public class SurveyResponse
+{
+    [Key]
+    public int Id { get; set; }
+    [Key]
+    public int SuveryAskId { get; set; }
+
+    public string value { get; set; }
+
+    public ICollection<ResponsePosibility> ResponsePosibilities { get; set; }
+}

8f6674fd166782e3c5db8ec0fafeeadc4404111d
Diego Alfonso Criado <diego.ac@factory.generalsoftwareinc.com>
2024-03-22 22:13:52 -0500

    migrations add descriptions properti into organization entity

---
+++ DataAcces/Migrations/20240323031158_add_descriptions_column_into_organization_table.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAcces.Migrations
+{
+    /// <inheritdoc />
+    public partial class add_descriptions_column_into_organization_table : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Organization",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Organization");
+        }
+    }
+}

c61820eab522c4c074a75e7678214293ea418993
Diego Alfonso Criado <diego.ac@factory.generalsoftwareinc.com>
2024-03-23 19:10:49 -0500

    migrations to add images properties into user and university tables

---
+++ DataAcces/Migrations/20240324000853_add_image_into_user_university_tables.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAcces.Migrations
+{
+    /// <inheritdoc />
+    public partial class add_image_into_user_university_tables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "BgImage",
+                table: "University",
+                type: "bytea",
+                nullable: false,
+                defaultValue: new byte[0]);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "University",
+                type: "character varying(25)",
+                maxLength: 25,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "ProfileImage",
+                table: "University",
+                type: "bytea",
+                nullable: false,
+                defaultValue: new byte[0]);
+
+            migrationBuilder.AddColumn<int>(
+                name: "facultiesNumber",
+                table: "University",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Image",
+                table: "AspNetUsers",
+                type: "bytea",
+                nullable: false,
+                defaultValue: new byte[0]);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BgImage",
+                table: "University");
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "University");
+
+            migrationBuilder.DropColumn(
+                name: "ProfileImage",
+                table: "University");
+
+            migrationBuilder.DropColumn(
+                name: "facultiesNumber",
+                table: "University");
+
+            migrationBuilder.DropColumn(
+                name: "Image",
+                table: "AspNetUsers");
+        }
+    }
+}

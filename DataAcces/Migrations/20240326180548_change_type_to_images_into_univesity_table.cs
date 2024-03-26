8ff1af03cadb65f932013bbeabddd64e5f70f06d
Diego Alfonso Criado <diego.ac@factory.generalsoftwareinc.com>
2024-03-26 13:06:45 -0500

    migration to change types of ProfileImage and BgImage properties of university entity

---
+++ DataAcces/Migrations/20240326180548_change_type_to_images_into_univesity_table.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAcces.Migrations
+{
+    /// <inheritdoc />
+    public partial class change_type_to_images_into_univesity_table : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ProfileImage",
+                table: "University",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(byte[]),
+                oldType: "bytea");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BgImage",
+                table: "University",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(byte[]),
+                oldType: "bytea");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "ProfileImage",
+                table: "University",
+                type: "bytea",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "BgImage",
+                table: "University",
+                type: "bytea",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+    }
+}
